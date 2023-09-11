7fef28b160b1d16c2081f57b368f7d95324f6817
mariano <mariano@oxxon-cd.com>
2023-09-11 16:12:16 -0300

    Remove decorator

--- Metafar.Challange.Entities/Api/V1/RetiroRequestModel.cs
+++ Metafar.Challange.Entities/Api/V1/RetiroRequestModel.cs
@@ -6,7 +6,6 @@ namespace Metafar.Challange.Entities.Api.V1
     {
         [Required]
         [Range(0.01, double.MaxValue, ErrorMessage = "El campo Monto debe ser mayor que 0")]
-        [Display(Name = "Monto")]
         public decimal Amount { get; set; }
     }
 }
